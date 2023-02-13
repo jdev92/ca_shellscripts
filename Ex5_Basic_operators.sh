4c424df81d67c6faa47a38d08918a3359c3f953c
jdev92 <juliencavaillon6@gmail.com>
2023-02-13 19:59:26 +0100

    Ex5_Basic_operators.sh

---
+++ Ex5_Basic_operators.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Exercice
+# In this exercise, you will need to calculate to total cost (variable TOTAL) of a fruit basket, which contains 1 pineapple, 2 bananas and 3 watermelons. Don't forget to include the cost of the basket....
+
+COST_PINEAPPLE=50
+COST_BANANA=4
+COST_WATERMELON=23
+COST_BASKET=1
+
+TOTAL=$(($COST_PINEAPPLE + (2 * $COST_BANANA) + (3 * $COST_WATERMELON) + $COST_BASKET))
+echo "Total Cost is $TOTAL"
