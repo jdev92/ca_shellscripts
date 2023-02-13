d529825eda5c1214b3f66c527ff6cff67aeba08e
jdev92 <juliencavaillon6@gmail.com>
2023-02-13 20:15:57 +0100

    Ex7_Decision_making.sh

---
+++ Ex7_Decision_making.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Exercise
+# Change the variables in the first section, so that each if statement resolves as True.
+# change these variables
+NUMBER=16
+APPLES=16
+KING=LUIS
+# modify above variables
+# to make all decisions below TRUE
+if [ $NUMBER -gt 15 ] ; then
+  echo 1
+fi
+if [ $NUMBER -eq $APPLES ] ; then
+  echo 2
+fi
+if [[ ($APPLES -eq 12) || ("$KING" = "LUIS") ]] ; then
+  echo 3
+fi
+if [[ $(($NUMBER + $APPLES)) -le 32 ]] ; then
+  echo 4
+fi
